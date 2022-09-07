a4143c0c15afb8468c3e6da38f9564133dbf8796
olinr122 <44399296+olinr122@users.noreply.github.com>
2022-09-07 22:02:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,17 +31,17 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "77",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "oFx5R5w2rAQafe2mqeWIVYksQyD0",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "jmuVXJwO4zXS_ajwGl7--4B-kUyF4IWAjGhXcpleyzc",
       // 所在省份
-      province: "广东",
+      province: "河北",
       // 所在城市
-      city: "肇庆",
+      city: "邯郸",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "http://xn--0tr73f59srhm6ham6ihw6a.xn--4gqz6tfur0m4arrc.xn--6qq986b3xl/"
     }, 
     {
       name: "老婆1", 
@@ -80,9 +80,9 @@ export const config = {
   CALLBACK_USERS: [
     {
       // 一般都填自己
-      name: "自己",
+      name: "olinr",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "oFx5R5w2rAQafe2mqeWIVYksQyD0",
     }, 
   ],
     
@@ -93,9 +93,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "河北",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "邯郸",
 
   /** 重要节日相关 */
 
